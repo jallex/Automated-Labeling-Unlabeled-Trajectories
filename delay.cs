130ddecfb235bc301111f627571f622c0961a1d2
Jackie Allex <44556715+jallex@users.noreply.github.com>
2020-03-21 19:58:31 -0400

    Delay animation by given # seconds

---
+++ delay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delay : MonoBehaviour
+{
+    public float startDelay;
+    public Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(DelayedAnimation());
+    }
+
+    // The delay coroutine
+    IEnumerator DelayedAnimation()
+    {
+        yield return new WaitForSeconds(startDelay);
+        animator.Play("scene-animation");
+    }
+}
